3ba1de1c6b3250deaeab4a1275b53e19d8518c71
Raman-Pandey <112771319+Raman-Pandey@users.noreply.github.com>
2023-01-27 12:04:58 +0530

    Add files via upload

---
+++ R/pack.R
@@ -0,0 +1,28 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' func
+#' @export
+#' @param v numeric
+
+basic_function = function(v){
+  data.frame(
+    min = min(v),
+    max = max(v),
+    range = max(v),
+    std_dev = sd(v)
+  )
+}
+
